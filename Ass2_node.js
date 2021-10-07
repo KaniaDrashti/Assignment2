2006a4342e7d6f378abc55f5cca90df1a7ee41b3
Drashti Kania <91381034+KaniaDrashti@users.noreply.github.com>
2021-10-07 16:59:58 +0000

    final run program

--- Ass2_node.js
+++ Ass2_node.js
@@ -1,8 +1,8 @@
 const os=require("os");
-Console.log("\n operating system architechture:" + os.arch());
-Console.log("\n free memory of the os:" + os.freemem()/(1024*1024*1024));
-Console.log("\n Total memory of os:" + os.totalmem()/(1024*1024*1024));
-Console.log("\n os platform:" + os.platform());
-Console.log("\n display info of current loggin user:");
-Console.log(os.userInfo());
+console.log("\n operating system architechture:" + os.arch());
+console.log("\n free memory of the os:" + os.freemem()/(1024*1024*1024));
+console.log("\n Total memory of os:" + os.totalmem()/(1024*1024*1024));
+console.log("\n os platform:" + os.platform());
+console.log("\n display info of current loggin user:");
+console.log(os.userInfo());
 
